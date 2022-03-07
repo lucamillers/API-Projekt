9ab525b538c1d8d626af85113a943f887c3450c2
Luca Millers <lucamillers@googlemail.com>
2022-03-07 23:21:11 +0100

    Added more endpoints: '/list/<list_id>/entry', methods=['POST'], '/users', methods=['GET'], '/todos', methods=['GET'], '/user', methods=['POST'], '/user/<user_id>', methods=['GET', 'DELETE']

--- apitest.py
+++ apitest.py
@@ -16,16 +16,16 @@ from flask import Flask, request, jsonify, abort
 app = Flask(__name__)
 
 # create unique id for lists, users, entries
-user_id_bob = uuid.uuid4()
-user_id_alice = uuid.uuid4()
-user_id_eve = uuid.uuid4()
+user_id_bob = str(uuid.uuid4())
+user_id_alice = str(uuid.uuid4())
+user_id_eve = str(uuid.uuid4())
 todo_list_1_id = '1318d3d1-d979-47e1-a225-dab1751dbe75'
 todo_list_2_id = '3062dc25-6b80-4315-bb1d-a7c86b014c65'
 todo_list_3_id = '44b02e00-03bc-451d-8d01-0c67ea866fee'
-todo_1_id = uuid.uuid4()
-todo_2_id = uuid.uuid4()
-todo_3_id = uuid.uuid4()
-todo_4_id = uuid.uuid4()
+todo_1_id = str(uuid.uuid4())
+todo_2_id = str(uuid.uuid4())
+todo_3_id = str(uuid.uuid4())
+todo_4_id = str(uuid.uuid4())
 
 # define internal data structures with example data
 user_list = [
@@ -76,6 +76,30 @@ def handle_list(list_id):
         todo_lists.remove(list_item)
         return '', 200
 
+# define endpoint for getting and deleting existing todo list entries
+@app.route('/list/<list_id>/entry', methods=['POST'])
+def add_new_entry(list_id):
+    # find todo list depending on given list id
+    list_item = None
+    for l in todo_lists:
+        if l['id'] == list_id:
+            list_item = l
+            break
+    # if the given list id is invalid, return status code 404
+    if not list_item:
+        abort(404)
+    if request.method == 'POST':
+    # make JSON from POST data (even if content type is not set correctly)
+        new_entry = request.get_json(force=True)
+        print('Got new entry to be added: {}'.format(new_entry))
+    # create id for new entry, save it and return the list with id
+        new_entry['id'] = str(uuid.uuid4())
+        new_entry['list'] = list_id
+        todos.append(new_entry)
+        print('Current Entries: {}'.format(todos))
+        return jsonify(new_entry), 200
+
+
 
 # define endpoint for adding a new list
 @app.route('/list', methods=['POST'])
@@ -93,6 +117,49 @@ def add_new_list():
 @app.route('/lists', methods=['GET'])
 def get_all_lists():
     return jsonify(todo_lists)
+
+# define endpoint for getting all users
+@app.route('/users', methods=['GET'])
+def get_all_users():
+    return jsonify(user_list)
+
+# define endpoint for getting all todos
+@app.route('/todos', methods=['GET'])
+def get_all_todos():
+    return jsonify(todos)
+
+# define endpoint for adding a new list
+@app.route('/user', methods=['POST'])
+def add_new_user():
+    # make JSON from POST data (even if content type is not set correctly)
+    new_user = request.get_json(force=True)
+    print('Got new user to be added: {}'.format(new_user))
+    # create id for new list, save it and return the list with id
+    new_user['id'] = str(uuid.uuid4())
+    user_list.append(new_user)
+    print('Current Users: {}'.format(user_list))
+    return jsonify(user_list), 200
+
+@app.route('/user/<user_id>', methods=['GET', 'DELETE'])
+def handle_user(user_id):
+    # find todo list depending on given list id
+    user_item = None
+    for l in user_list:
+        if l['id'] == user_id:
+            user_item = l
+            break
+    # if the given list id is invalid, return status code 404
+    if not user_item:
+        abort(404)
+    if request.method == 'GET':
+        # find all todo entries for the todo list with the given id
+        print('Returning user list...')
+        return jsonify([i for i in user_list if i['id'] == user_id])
+    elif request.method == 'DELETE':
+        # delete list with given id
+        print('Deleting user from list...')
+        user_list.remove(user_item)
+        return jsonify(user_list), 200
 
 
 if __name__ == '__main__':
